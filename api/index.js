adbb033811e013ff6264f030971562daf584d2b1
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-11-16 19:45:50 +0500

    udpates_again

---
+++ api/index.js
@@ -0,0 +1,48 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+const connectionString = process.env.MONGODB_CONNECT_URI || '';
+
+const client = new MongoClient(connectionString);
+
+let conn;
+try {
+    conn = await client.connect();
+} catch (e) {
+    console.error(e);
+}
+
+let db = conn.db('Linux_For_Dummies');
+
+export default db;
+
+
+app.get("/login", async (req, res) => {
+  try {
+    const collection = db.collection('Credentials');
+    collection.find({}).toArray().then((result) => {
+      console.log(result[0]);
+
+      if (!result || result.length === 0) {
+        res.status(404).json({ message: 'Data not found' });
+      } else {
+        res.status(200).json(result);
+      }
+    }).catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+  });
+
+app.listen(5000, () => console.log('Server ready at http://localhost:5000'));
