b2653d62bbecf029bba7f94d836b4a2469b7d9a5
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-11-16 19:23:51 +0500

    dubara koshish

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ app.get("/login", async (req, res) => {
   try {
     const db = mongoose.connection.useDb("Linux_For_Dummies")
     const collection = db.collection('Credentials');
-    const result = await collection.find({}).toArray();
-    console.log(result[0]);
-  
-    if (!result) {
-      res.status(404).json({ message: 'Data not found' });
-    } else {
-      res.status(200).json(result);
-    }
+    collection.find({}).toArray().then((result) => {
+      console.log(result[0]);
+
+      if (!result || result.length === 0) {
+        res.status(404).json({ message: 'Data not found' });
+      } else {
+        res.status(200).json(result);
+      }
+    }).catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal Server Error' });
